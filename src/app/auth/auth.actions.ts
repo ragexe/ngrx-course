e69d5fb4aac5a1b6f4507bf0919cf7290f83aa27
Raman Makeichyk <ragexeone@gmail.com>
2022-09-20 10:33:45 +0400

    chore: prettify `src`

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,14 +1,6 @@
-import {createAction, props} from '@ngrx/store';
-import {User} from './model/user.model';
+import { createAction, props } from '@ngrx/store'
+import { User } from './model/user.model'
 
+export const login = createAction('[Login Page] User Login', props<{ user: User }>())
 
-export const login = createAction(
-    "[Login Page] User Login",
-    props<{user: User}>()
-);
-
-
-
-export const logout = createAction(
-  "[Top Menu] Logout"
-);
+export const logout = createAction('[Top Menu] Logout')
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,18 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LoginComponent} from './login/login.component';
-import {MatCardModule} from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
-import {RouterModule} from "@angular/router";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatButtonModule} from "@angular/material/button";
-import { StoreModule } from '@ngrx/store';
-import {AuthService} from "./auth.service";
-import * as fromAuth from './reducers';
-import {authReducer} from './reducers';
-import {AuthGuard} from './auth.guard';
-import {EffectsModule} from '@ngrx/effects';
-import {AuthEffects} from './auth.effects';
+import { ModuleWithProviders, NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { LoginComponent } from './login/login.component'
+import { MatCardModule } from '@angular/material/card'
+import { MatInputModule } from '@angular/material/input'
+import { RouterModule } from '@angular/router'
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button'
+import { StoreModule } from '@ngrx/store'
+import { AuthService } from './auth.service'
+import * as fromAuth from './reducers'
+import { authReducer } from './reducers'
+import { AuthGuard } from './auth.guard'
+import { EffectsModule } from '@ngrx/effects'
+import { AuthEffects } from './auth.effects'
 
 @NgModule({
     imports: [
@@ -21,21 +21,18 @@ import {AuthEffects} from './auth.effects';
         MatCardModule,
         MatInputModule,
         MatButtonModule,
-        RouterModule.forChild([{path: '', component: LoginComponent}]),
+        RouterModule.forChild([{ path: '', component: LoginComponent }]),
         StoreModule.forFeature('auth', authReducer),
-        EffectsModule.forFeature([AuthEffects])
+        EffectsModule.forFeature([AuthEffects]),
     ],
     declarations: [LoginComponent],
-    exports: [LoginComponent]
+    exports: [LoginComponent],
 })
 export class AuthModule {
     static forRoot(): ModuleWithProviders<AuthModule> {
         return {
             ngModule: AuthModule,
-            providers: [
-              AuthService,
-                AuthGuard
-            ]
+            providers: [AuthService, AuthGuard],
         }
     }
 }
